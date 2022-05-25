321312ff75adbd160eb5516be05413305b669b42
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-05-25 23:56:05 +0530

    inital commit

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchBar from "./SearchBar";
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+class SearchBar extends React.Component {
+  state = { term: "" };
+
+  onInputChange = (e) => {
+    this.setState({ term: e.target.value });
+  };
+
+  onFormSubmit = (e) => {
+    e.preventDefault();
+  };
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label> video search </label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+export default SearchBar;
