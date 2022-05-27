5174f1dfd3a91eb4db075f9d6ead9f1b8391b1b2
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-05-27 23:25:24 +0530

    updated videoList

--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -1,8 +1,8 @@
 import React from "react";
-
+import "./VideoItem.css";
 const VideoItem = ({ video }) => {
   return (
-    <div className="item">
+    <div className="videoItem item">
       <img className="ui image" src={video.snippet.thumbnails.medium.url} />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
