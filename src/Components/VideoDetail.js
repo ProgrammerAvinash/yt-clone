5fad195608c4cb830d6e0afad1f5470759a8039b
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-05-30 02:19:15 +0530

    completed V1

--- src/Components/VideoDetail.js
+++ src/Components/VideoDetail.js
@@ -4,7 +4,17 @@ const VideoDetail = ({ video }) => {
   if (!video) {
     return <div>loading...</div>;
   }
-  return <div> {video.snippet.title}</div>;
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+  return (
+    <div>
+      <div className="ui embed">
+        <iframe src={videoSrc} />
+      </div>
+      <div className="ui segment">
+        <h4>{video.snippet.title}</h4>
+      </div>
+    </div>
+  );
 };
 
 export default VideoDetail;
