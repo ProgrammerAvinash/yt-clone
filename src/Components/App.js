022b2ff806df64cb84aa35ee09205b264e487c49
Avinash Yadav <62152948+ProgrammerAvinash@users.noreply.github.com>
2022-05-29 23:58:49 +0530

    ChangesMade

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,7 +4,7 @@ import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
@@ -14,11 +14,18 @@ class App extends React.Component {
     });
     this.setState({ videos: response.data.items });
   };
+
+  onVideoSelect = (video) => {
+    console.log();
+  };
   render() {
     return (
       <div>
         <SearchBar onTermSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
--- src/Components/VideoItem.js
+++ src/Components/VideoItem.js
@@ -1,8 +1,8 @@
 import React from "react";
 import "./VideoItem.css";
-const VideoItem = ({ video }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
-    <div className="videoItem item">
+    <div onClick={() => onVideoSelect(video)} className="videoItem item">
       <img className="ui image" src={video.snippet.thumbnails.medium.url} />
       <div className="content">
         <div className="header">{video.snippet.title}</div>
